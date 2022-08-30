2a3c70f53d6eb002696f239bcf3e8ab8592af6e6
“mmpyo157” <miiii12150921@gmail.com>
2022-08-30 15:32:59 +0900

    [add]画面遷移の実装

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.firstapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//次回は正しいブランチに画面実装を入れる
+        val nextActivity_btn = findViewById<Button>(R.id.btn_nextActivity)    //画面のボタン
+        nextActivity_btn.setOnClickListener {
+            //Intentオブジェクト生成、遷移画面定義
+            val nextIntent = Intent(this, ColorActivity::class.java)
+            //Intentオブジェクトにテキストの内容をプットする
+//            nextIntent.putExtra("main_tv", main_tv.text.toString())
+            //次のActivity実行
+            startActivity(nextIntent)
+        }
     }
 }
