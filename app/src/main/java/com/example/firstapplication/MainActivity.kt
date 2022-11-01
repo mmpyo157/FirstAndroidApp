8dcfb6039de13ed0c1af18074c1d49c8ee59a3f7
ikedamiho <miiii12150921@gmail.com>
2022-11-01 13:05:49 +0900

    [fix]ダイアログ実装

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.firstapplication
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -32,5 +34,21 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
                     startActivity(intent)
         }
+
+        val dialog_btn = findViewById<Button>(R.id.btn_dialog)    //画面のボタン
+        dialog_btn.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage(R.string.dialog_start_game)
+                .setPositiveButton(R.string.start,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // START THE GAME!
+                    })
+                .setNegativeButton(R.string.cancel,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // User cancelled the dialog
+                    })
+            // Create the AlertDialog object and return it
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
     }
 }
